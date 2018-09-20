df5e04adf7621680e7700741b2b1d3516b4acf36
Gabriel Arce <gab.arpe@gmail.com>
2018-09-20 20:46:34 -0300

    Test cel call feature

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
+
 const port = process.env.PORT || 3000
 
 
 const publicPath = path.join(__dirname,'../public')
 
 var app = express()
+var server = http.createServer(app)
+
+var io = socketIO(server)
+
+io.on('connection', (socket)=>{
+    console.log('New User connected')
+})
+
 app.use(express.static(publicPath))
 
-app.listen(port,()=>console.log(`Listening port ${port}`))
+server.listen(port,()=>console.log(`Listening port ${port}`))
