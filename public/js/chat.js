dd8f9c84d59642da3c80b0a512423b6758c1d0ea
Gabriel Arce <gab.arpe@gmail.com>
2018-09-24 17:13:21 -0300

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -106,7 +106,7 @@ $('#message-form').on('submit',function(e){
     e.preventDefault()
     var messageTextbox = $('[name=message]')
     socket.emit('createMessage',{
-        from: 'User',
+      
         text: messageTextbox.val()
 
     },function(){
