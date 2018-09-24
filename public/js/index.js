b95858576783a26b84e279e80d7d4d523ec5dc5f
Gabriel Arce <gab.arpe@gmail.com>
2018-09-24 12:25:24 -0300

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom(){
+    //Selectors
+    var messages = $('#messages')
+    var newMessage = messages.children('li:last-child')
+
+    //Heights
+    var clientHeight = messages.prop('clientHeight')
+    var scrollTop = messages.prop('scrollTop')
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight()
+    var lastMessageHeight = newMessage.prev().innerHeight()
+
+    if(clientHeight+scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight)
+    }
+}
+
+
 socket.on('connect', function (){
     console.log('Connected to server')
    
@@ -25,6 +43,7 @@ socket.on('newMessage', function(message){
     })
 
     $("#messages").append(html)
+    scrollToBottom()
 
     /* var formattedTime = moment(message.createdAt).format('h:mm a')
     var li = $('<li></li>')
@@ -45,6 +64,7 @@ socket.on('newLocationMessage',function(message){
     })
 
     $("#messages").append(html)
+    scrollToBottom()
 
    /*  $("#messages").append(html)
     var formattedTime = moment(message.createdAt).format('h:mm a')
